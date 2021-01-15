4c59a80d84dd62cd23936dcd26fe77d9b447620e
Mariusz <urban.mariusz@icloud.com>
2021-01-15 17:11:46 +0100

    done

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
 
 export default function NewPlace() {
-    return (
-        <h2>New Place Works! </h2>
-    )
+  return (
+    <form action="" className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter valid title"
+      />
+    </form>
+  );
 }
